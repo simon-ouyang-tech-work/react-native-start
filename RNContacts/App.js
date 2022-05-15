ec379bd86dbe6069b254fd114105a86819bcf893
ouyang simon <xi.ouyang@us.nationalgrid.com>
2022-05-15 13:52:15 -0700

    update code

--- RNContacts/App.js
+++ RNContacts/App.js
@@ -1,39 +1,100 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React, {useState} from 'react';
-import {StyleSheet, Text, View, Button} from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  Pressable,
+  Alert,
+  ToastAndroid,
+} from 'react-native';
 
 const App = () => {
-  const [name, setName] = useState('Simon');
-  const onClickHandler = () => {
-    setName('Programming with Simon');
+  const [name, SetName] = useState('');
+  const [submitted, SetSubmitted] = useState(false);
+  const onPressHandler = () => {
+    if (name.length > 3) {
+      SetSubmitted(!submitted);
+    } else {
+      // Alert.alert(
+      //   'Warning',
+      //   'The name must be longer than 3 characters', [
+      //   {
+      //     text: 'Do not show again',
+      //     onPress: () => console.warn('Do not show Pressed!')
+      //   },
+      //   {
+      //     text: 'Cancel',
+      //     onPress: () => console.warn('Cancel Pressed!')
+      //   },
+      //   {
+      //     text: 'OK',
+      //     onPress: () => console.warn('OK Pressed!')
+      //   },
+      // ],
+      //   {
+      //     cancelable: true,
+      //     onDismiss: () => console.warn('Alert dismissed!')
+      //   })
+      ToastAndroid.showWithGravity(
+        'The name must be longer than 3 characters',
+        ToastAndroid.LONG,
+        ToastAndroid.CENTER,
+      );
+    }
   };
+
   return (
     <View style={styles.body}>
-      <Text style={styles.text}>{name}</Text>
-      <Button title="Press Me!" onPress={onClickHandler} />
+      <Text style={styles.text}>Please write your name:</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="e.g. John"
+        onChangeText={value => SetName(value)}
+      />
+      <Pressable
+        onPress={onPressHandler}
+        hitSlop={{top: 10, bottom: 10, right: 10, left: 10}}
+        android_ripple={{color: '#00f'}}
+        style={({pressed}) => [
+          {backgroundColor: pressed ? '#dddddd' : '#00ff00'},
+          styles.button,
+        ]}>
+        <Text style={styles.text}>{submitted ? 'Clear' : 'Submit'}</Text>
+      </Pressable>
+      {submitted ? (
+        <Text style={styles.text}>You are registered as {name}</Text>
+      ) : null}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   body: {
+    flex: 1,
     backgroundColor: '#ffffff',
     alignItems: 'center',
     justifyContent: 'center',
-    flex: 1,
   },
   text: {
-    color: '#ffffff',
+    color: '#000000',
     fontSize: 20,
-    fontStyle: 'italic',
     margin: 10,
+  },
+  input: {
+    width: 200,
+    borderWidth: 1,
+    borderColor: '#555',
+    borderRadius: 5,
+    textAlign: 'center',
+    fontSize: 20,
+    marginBottom: 10,
+  },
+  button: {
+    width: 150,
+    height: 50,
+    alignItems: 'center',
+    borderRadius: 20,
   },
 });
 
